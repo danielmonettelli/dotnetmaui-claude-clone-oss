cc1a25ed1b36783f817668bbe0dce12d43484ff5
Daniel Monettelli <danielmonetelli@hotmail.com>
2025-02-12 22:47:14 -0500

    🚀 [Code] Updated font configurations to replace Open Sans with Poppins and Nimbus Roman fonts

--- Claude/MauiProgram.cs
+++ Claude/MauiProgram.cs
@@ -11,12 +11,14 @@ namespace Claude
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Poppins-Regular.ttf", "Poppins#400");
+                    fonts.AddFont("Poppins-SemiBold.ttf", "Poppins#600");
+                    fonts.AddFont("NimbusRomNo9L-Reg.ttf", "NimbusRomanNo9L#400");
+                    fonts.AddFont("NimbusRomNo9L-Bold.ttf", "NimbusRomanNo9L#700");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
